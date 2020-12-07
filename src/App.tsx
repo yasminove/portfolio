7904531e5e62805688399083c473f24688592b2e
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-07 12:17:12 +0200

    top frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import ProfilePic from './images/yasmin.jpeg'
+
 function App() {
   return (
     <div className='App'>
@@ -16,10 +18,35 @@ function App() {
               <div id='dot-2' className='browser-dot'></div>
               <div id='dot-3' className='browser-dot'></div>
             </div>
-            <ul id="navigation">NAV</ul>
+            <ul id="navigation">
+              <li><a href="#">Contact</a></li>
+            </ul>
             </div>
-          <div className="left-column">left-column</div>
-          <div className="right-column">right-column</div>
+         
+          <div className="left-column">
+            <img id="profile-pic" src={ProfilePic}/>
+            <h5 style={{textAlign: 'center', lineHeight: 0}}>Personalize Theme</h5>
+            <div id="theme-options-wrapper">
+              <div id="light-mode" className="theme-dot"></div>
+              <div id="blue-mode" className="theme-dot"></div>
+              <div id="green-mode" className="theme-dot"></div>
+              <div id="purple-mode" className="theme-dot"></div>
+           
+            </div>
+            <p id="settings-note">Theme options will be saved <br />for your next visit</p>
+          </div>
+          <div className="right-column">
+            <div>
+              <div id="preview">
+                <div id="corner-tl" className="corner"></div>
+                <div id="corner-tl" className="corner"></div>
+                <div id="corner-tl" className="corner"></div>
+                <div id="corner-tl" className="corner"></div>
+              <h3>What I Do</h3>
+                <p> I am a Web Developer</p>
+              </div>
+            </div>
+          </div>
         </div>
         </div>
 
